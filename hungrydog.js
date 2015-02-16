339d85fb401e65842bd6251d13b02f241efa1194
morganmccrory <morganmccrory@gmail.com>
2015-02-16 14:25:08 -0600

    Add puppy

--- hungrydog.js
+++ hungrydog.js
@@ -1,45 +1,108 @@
-function leftArrowPressed() {
-                var element = document.getElementById("puppy");
-                element.style.left = parseInt(element.style.left) - 5 + 'px';
-            }
+document.onkeydown = move;
 
-            function rightArrowPressed() {
-                var element = document.getElementById("puppy");
-                element.style.left = parseInt(element.style.left) + 5 + 'px';
-            }
+var puppy = {
+  x: 40,
+  y: 200,
+  alive: true,
+  won: false,
+  canMove: true,
+};
 
-            function upArrowPressed() {
-                var element = document.getElementById("puppy");
-                element.style.top = parseInt(element.style.top) - 5 + 'px';
-            }
+var alien = {
+  x: 560,
+  y: 80,
+  alive: true,
+};
 
-            function downArrowPressed() {
-                var element = document.getElementById("puppy");
-                element.style.top = parseInt(element.style.top) + 5 + 'px';
-            }
+var alien2 = {
+  x: 560,
+  y: 200,
+  alive: true,
+};
 
-            function moveSelection(event) {
-                switch (event.keyCode) {
-                    case 37:
-                        leftArrowPressed();
-                    break;
+var alien3 = {
+  x: 560,
+  y: 320,
+  alive: true,
+};
 
-                    case 39:
-                        rightArrowPressed();
-                    break;
+document.getElementById("up").onclick = function() {move(38);};
+document.getElementById("down").onclick = function() {move(40);};
+document.getElementById("right").onclick = function() {move(39);};
+document.getElementById("left").onclick = function() {move(37);};
 
-                    case 38:
-                        upArrowPressed();
-                    break;
+var puppyImg = document.getElementById("puppy");
+var alienImg = document.getElementById("alien");
+var alienImg2 = document.getElementById("alien2");
+var alienImg3 = document.getElementById("alien3");
 
-                    case 40:
-                        downArrowPressed();
-                    break;
-                }
-            };
 
-        function gameLoop()
-        {
-            // change position based on speed
-            moveSelection();
-            setTimeout("gameLoop()",10);
+
+var alienCount = 3;
+
+function move(direction){
+  if (puppy.canMove){
+    console.log('moving');
+    ai_move(alien, alienImg);
+    ai_move(alien2, alienImg2);
+    ai_move(alien3, alienImg3);
+    if (direction.keyCode === 38 || direction === 38 || direction.keyCode === 73){ // move up
+      if (puppy.y > 0){
+        puppy.y = (puppy.y - 40);
+        puppyImg.style.top = (puppy.y + "px");
+      }
+    }else if (direction.keyCode === 40 || direction === 40 || direction.keyCode === 75){ // move down
+      if (puppy.y < 440){
+        puppy.y = (puppy.y + 40);
+        puppyImg.style.top = (puppy.y + "px");
+      }
+    }else if (direction.keyCode === 39 || direction == 39 || direction.keyCode === 76){ // move right
+      if (puppy.x < 600){
+        puppy.x = (puppy.x + 40);
+        puppyImg.style.left = (puppy.x + "px");
+      }
+
+    }else if (direction.keyCode === 37 || direction == 37 || direction.keyCode === 74){ // move left
+      if (puppy.x > 0){
+        puppy.x = (puppy.x - 40);
+        puppyImg.style.left = (puppy.x + "px");
+      }
+    }
+    status(alien, alienImg);
+    status(alien2, alienImg2);
+    status(alien3, alienImg3);
+    setTimeout(function(){
+      puppy.canMove = true;
+    }, 200)
+    puppy.canMove = false;
+  }
+}
+
+function ai_move(player, playerImg){
+  var random = Math.floor((Math.random() * 4) + 1);
+  // move up
+  if (random === 1){
+    if (player.y > 0){
+      player.y = (player.y - 40);
+      playerImg.style.top = (player.y + "px");
+    }
+  }else if (random === 2){
+    // move down
+    if (player.y < 440){
+      player.y = (player.y + 40);
+      playerImg.style.top = (player.y + "px");
+    }
+  }else if (random === 3){
+    // move right
+    if (player.x < 600){
+      player.x = (player.x + 40);
+      playerImg.style.left = (player.x + "px");
+    }
+ }else{
+    //move left
+    if (player.x > 0){
+      player.x = (player.x - 40);
+      playerImg.style.left = (player.x + "px");
+    }
+ }
+}
