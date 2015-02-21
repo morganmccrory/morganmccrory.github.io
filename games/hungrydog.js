5b4257275be856a8ec2ab83fe1591c60bbb6392b
morganmccrory <morganmccrory@gmail.com>
2015-02-20 21:47:08 -0600

    Change position of all the food

--- games/hungrydog.js
+++ games/hungrydog.js
@@ -39,6 +39,11 @@ var boneCount = 3;
 function move(direction){
   if (puppy.canMove){
     console.log('moving');
+    food_move(bone1, boneImg1);
+    food_move(bone2, boneImg2);
+    food_move(bacon, baconImg);
+  if (puppy.canMove){
+    console.log('moving');
     if (direction.keyCode === 38 || direction === 38){ // move up
       if (puppy.y > 50){
         puppy.y = (puppy.y - 10);
@@ -62,6 +67,35 @@ function move(direction){
       }
     }
   }
+}
+
+function food_move(player, playerImg){
+  var random = Math.floor((Math.random() * 4) + 1);
+  // move up
+  if (random === 1){
+    if (player.y > 0){
+      player.y = (player.y - 40);
+      playerImg.style.top = (player.y + "px");
+    }
+  }else if (random === 2){
+    // move down
+    if (player.y < 440){
+      player.y = (player.y + 40);
+      playerImg.style.top = (player.y + "px");
+    }
+  }else if (random === 3){
+    // move right
+    if (player.x < 600){
+      player.x = (player.x + 40);
+      playerImg.style.left = (player.x + "px");
+    }
+ }else{
+    //move left
+    if (player.x > 0){
+      player.x = (player.x - 40);
+      playerImg.style.left = (player.x + "px");
+    }
+ }
 }
 
 document.onkeydown = move;
